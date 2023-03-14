dd52ccddf7341c8a3dc2dbf87d22f75e37744c5d
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-03-13 20:35:28 -0400

    🚧 • Added some fixes

--- client.js
+++ client.js
@@ -7,7 +7,13 @@ wm.style.height="45px";
 wm.style.bottom="10px";
 wm.style.left="10px";
 wm.style.cursor="pointer";
+wm.style.zIndex="25600";
+wm.id="foxdevwm";
+wm.style.transition="all .3s";
 wm.addEventListener("click", ofds);
+let wms = document.createElement('style');
+wms.innerHTML="scale:1.1;transform:translate(3px, -3px)";
+document.head.appendChild(wms);
 document.body.appendChild(wm);
 function ofds(){
   window.open('https://itsfoxdev.github.io', '_blank').focus();
