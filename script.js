ab2d80d769a46d0ae8c51b16f12c6aaf73e4f139
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-04-29 20:57:39 -0400

    🎉 Complete redesign

--- script.js
+++ script.js
@@ -1,50 +1,53 @@
-function goto(url){window.open(url, '_blank').focus()}
-function addItem(name,desc,iurl,murl,icos){
-  let adding = document.createElement('div');
-  adding.classList.add('item');
-  adding.innerHMTL = '<img class="thumb" src="https://github.com/ItsFoxDev/itsfoxdev.github.io/blob/main/res/creations/' + iurl + '.png?raw=true"></img><div class="popout"><span class="title">' + name + '</span><span class="desc">' + desc + '</span><span class="icos">' + icos + '</span></div>';
-  adding.onclick='goto(' + murl + ')';
-  /*adding.href=murl;
-  adding.target="_blank"; /**/
-   adding.addEventListener('click', function(){goto(murl)}); /**/
-  
-  let thumb = document.createElement('img');
-  thumb.classList.add('thumb');
-  thumb.src= "https://github.com/ItsFoxDev/itsfoxdev.github.io/blob/main/res/creations/" + iurl + ".png?raw=true"
-  adding.appendChild(thumb);
-  
-  let popout = document.createElement('div');
-  popout.classList.add('popout');
-  
-  let title = document.createElement('span');
-  title.classList.add('title');
-  title.innerHTML = name;
-  popout.appendChild(title);
-  
-  let descr = document.createElement('span');
-  descr.classList.add('desc');
-  descr.innerHTML = desc;
-  popout.appendChild(descr);
-  
-  let iconc = document.createElement('span');
-  iconc.classList.add('icos');
-  iconc.innerHTML = icos;
-  popout.appendChild(iconc);
-  
-  adding.appendChild(popout);
-  document.getElementById('items').appendChild(adding);
-  console.log('✅ • Added item "' + name + '" to gallery');
+function redir(u){
+  document.getElementById('wipe').style.height='100vh';
+  setTimeout(function(){document.location.href=u},300);
 }
-function solo(name,desc,iurl,murl,i1,l1){let icons = '<i onclick="event.stopPropagation();goto(`' + l1 + '`)"class="' + i1 + '"></i>';addItem(name,desc,iurl,murl,icons);}
-function duo(name,desc,iurl,murl,i1,l1,i2,l2){let icons = '<i onclick="event.stopPropagation();goto(`' + l1 + '`)"class="' + i1 + '"></i><i onclick="event.stopPropagation();goto(`' + l2 + '`)"class="' + i2 + '"></i>';addItem(name,desc,iurl,murl,icons);}
-function trio(name,desc,iurl,murl,i1,l1,i2,l2,i3,l3){let icons = '<i onclick="event.stopPropagation();goto(`' + l1 + '`)"class="' + i1 + '"></i><i onclick="event.stopPropagation();goto(`' + l2 + '`)"class="' + i2 + '"></i><i onclick="event.stopPropagation();goto(`' + l3 + '`)"class="' + i3 + '"></i>';addItem(name,desc,iurl,murl,icons);}
+function addMC(o){
+  o.addEventListener('auxclick', function(e) {
+    if (e.button == 1) {
+      alert("middle button clicked")
+    }
+  })
+}
+let tiles = document.querySelectorAll('div.tile[url]');
+tiles.forEach(element => addMC(element));
 
-duo('Auxilla','An online audio mixer','auxilla','https://itsfoxdev.github.io/proj/auxilla','fas fa-arrow-up-right-from-square','https://itsfoxdev.github.io/proj/auxilla','fas fa-code','https://scratch.mit.edu/projects/758502999/')
-duo('Time Warp','An online time warp','timewarp','https://itsfoxdev.github.io/proj/timewarp','fas fa-arrow-up-right-from-square','https://itsfoxdev.github.io/proj/timewarp','fas fa-code','https://scratch.mit.edu/projects/706528790/')
-solo('FoxTools','A Shortcuts multitool','foxtools-sc','https://www.icloud.com/shortcuts/b3287780ddf74736ac65e20f6569cf9a','fa-solid fa-cloud-arrow-down','https://www.icloud.com/shortcuts/b3287780ddf74736ac65e20f6569cf9a');
-solo('Userscripts','Useful mini-scripts','userscripts','https://itsfoxdev.github.io/Userscripts/','fa-solid fa-arrow-down-to-line','https://itsfoxdev.github.io/Userscripts/');
-duo('FoxClient','An Eaglercraft fork','foxclient','https://itsfoxdev.github.io/FoxClient/1.5/','fas fa-arrow-up-right-from-square','https://itsfoxdev.github.io/FoxClient/1.5/','fas fa-code','https://github.com/ItsFoxDev/itsfoxdev.github.io/tree/main/FoxClient/1.5/')
-solo('fa-turbo','Fast Font Awesome','fa-turbo','https://github.com/ItsFoxDev/fa-turbo','fa-brands fa-github','https://github.com/ItsFoxDev/fa-turbo/');
-solo('FoxCMD','A CLI for macOS','foxcmd','https://github.com/ItsFoxDev/FoxCMD','fa-brands fa-github','https://github.com/ItsFoxDev/FoxCMD/');
-solo('Separator','For your bookmarks','separator','https://itsfoxdev.github.io/Separator','fas fa-arrow-up-right-from-square','https://itsfoxdev.github.io/Separator');
-duo('PasteZone','Temporary text editor','pastezone','https://itsfoxdev.github.io/PasteZone','fas fa-arrow-up-right-from-square','https://itsfoxdev.github.io/PasteZone','fas fa-code','https://github.com/ItsFoxDev/PasteZone')
+
+const trailer = document.getElementById("trailer");
+const animateTrailer = (e) => {
+  const x = e.clientX - trailer.offsetWidth / 2,
+        y = e.clientY - trailer.offsetHeight / 2;
+  
+  const keyframes = {
+    transform: `translate(${x}px, ${y}px)`
+  }
+  trailer.animate(keyframes, { 
+    duration: 10000, 
+    fill: "forwards" 
+  });
+}
+window.onmousemove = e => {
+  animateTrailer(e);
+}
+
+function checkURL() {
+	if (window.location.href.includes('#launcher')){
+    document.getElementById('app-launcher').style.scale="1";
+    document.getElementById('app-launcher').style.height=null;
+    document.getElementById('app-home').style.scale="0";
+    document.getElementById('app-home').style.height="0px";
+    document.querySelector('a[href="#launcher"]').classList.add('selected');
+    document.querySelector('a[href="#home"]').classList.remove('selected');
+    document.title="Launcher";
+  } else {
+    document.getElementById('app-launcher').style.scale="0";
+    document.getElementById('app-launcher').style.height="0px";
+    document.getElementById('app-home').style.scale="1";
+    document.getElementById('app-home').style.height=null;
+    document.querySelector('a[href="#launcher"]').classList.remove('selected');
+    document.querySelector('a[href="#home"]').classList.add('selected');
+    document.title="FoxDev";
+  }
+}
+checkURL();
+window.addEventListener('popstate', function(){checkURL()});
