c3ea4552e821c021119e0ad741f17659cb30228d
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2024-01-27 21:32:01 -0500

    🎉  Added more to about me section

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
 document.querySelectorAll('button').forEach((e) => {
-  var img = e.getAttribute('imgsrc');
-  var txt = e.getAttribute('label');
-  e.innerHTML='<div class="imgs"><img width="40" height="40" src="'+img+'"><img class="blur" width="40" height="40" src="'+img+'"></div>'+txt;
+  if (!e.classList.contains('ns')){
+    var img = e.getAttribute('imgsrc');
+    var txt = e.getAttribute('label');
+    if (e.getAttribute('sublabel')){
+      sl = e.getAttribute('sublabel');
+      e.innerHTML='<div class="imgs"><img width="40" height="40" src="'+img+'"><img class="blur" width="40" height="40" src="'+img+'"></div><div class="txts"><span class="tmain">'+txt+'</span><span class="tsub">'+sl+'</span></span>';
+    } else {
+      e.innerHTML='<div class="imgs"><img width="40" height="40" src="'+img+'"><img class="blur" width="40" height="40" src="'+img+'"></div>'+txt;
+    }
+  }
 });
+
+function cc1(e,l){
+  document.querySelectorAll('.ab1').forEach(function(f){f.classList.remove('ab1')})
+  e.classList.add('ab1')
+}
